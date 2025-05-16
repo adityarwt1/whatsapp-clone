5b9aec4d5cbfcdaeac35b5ac771bb4df5b782cce
adityarwt1 <adityarawatnew2487@gmail.com>
2025-05-16 15:02:40 +0530

    this hyphone

--- app/api/addchat/route.ts
+++ app/api/addchat/route.ts
@@ -1,48 +1,48 @@
-import { getUser } from "@/actions/my-action";
-import { PrismaClient } from "@/app/generated/prisma";
-import { redirect } from "next/navigation";
-import { NextRequest, NextResponse } from "next/server";
+  import { getUser } from "@/actions/my-action";
+  import { PrismaClient } from "@/app/generated/prisma";
+  import { redirect } from "next/navigation";
+  import { NextRequest, NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
+  const prisma = new PrismaClient();
 
-export async function POST(req: NextRequest) {
-  try {
-    const user = await getUser();
+  export async function POST(req: NextRequest) {
+    try {
+      const user = await getUser();
 
-    if (!user) {
-      redirect("/login");
-    }
-    const { users } = await req.json();
-    console.log(users);
+      if (!user) {
+        redirect("/login");
+      }
+      const { users } = await req.json();
+      console.log(users);
 
-    const chat = await prisma.chat.create({
-      data: {
-        name: users?.firstName,
-        isGroup: false,
-        createdAt: new Date(),
-        createdBy: user?.id,
-        picture: users?.profilePicture,
-      },
-    });
+      const chat = await prisma.chat.create({
+        data: {
+          name: users?.firstName,
+          isGroup: false,
+          createdAt: new Date(),
+          createdBy: user?.id,
+          picture: users?.profilePicture,
+        },
+      });
 
-    if (!chat) {
-      console.log("unable to make the chat");
+      if (!chat) {
+        console.log("unable to make the chat");
+        return NextResponse.json(
+          { message: "unable to make the user" },
+          { status: 403 }
+        );  
+      }         
+
       return NextResponse.json(
-        { message: "unable to make the user" },
-        { status: 403 }
+        { message: "SuccessFully chat created" },
+        { status: 201 }
+      );
+    } catch (error) {
+      console.log(error);
+
+      return NextResponse.json(
+        { message: "Internal Server issue" },
+        { status: 500 }
       );
     }
-
-    return NextResponse.json(
-      { message: "SuccessFully chat created" },
-      { status: 201 }
-    );
-  } catch (error) {
-    console.log(error);
-
-    return NextResponse.json(
-      { message: "Internal Server issue" },
-      { status: 500 }
-    );
   }
-}
